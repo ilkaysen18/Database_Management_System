3491bc5d3af804a228178d649f9dc244f99e0f4a
Ilkay Rachel Lee Sen <ilkaysen18@gmail.com>
2026-09-10 22:43:04 +0300

    Update 01_schema.sql

--- Development_P2/scripts/01_schema.sql
+++ Development_P2/scripts/01_schema.sql
@@ -1,1 +1,20 @@
+-- ============================================
+--      1. BASE / INDEPENDENT ENTITIES
+-- ============================================
 
+-- Create main "User" Entity Table 
+CREATE TABLE "User" (
+  "User_ID" INT GENERATED ALWAYS AS IDENTITY,
+  "User_Name",
+  "User_Email",
+  "User_Phone",
+  "Password_Hash",
+
+-- Define Primary Key (PK) constraint
+
+
+  
+
+-- ============================================
+--           2. DEPENDENT ENTITIES
+-- ============================================
