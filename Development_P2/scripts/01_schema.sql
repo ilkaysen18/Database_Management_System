d9be52c50486f9139a32ab4f7a7b34669458bbc9
Ilkay Rachel Lee Sen <ilkaysen18@gmail.com>
2026-09-10 23:03:34 +0300

    Update 01_schema.sql

--- Development_P2/scripts/01_schema.sql
+++ Development_P2/scripts/01_schema.sql
@@ -5,16 +5,34 @@
 -- Create main "User" Entity Table 
 CREATE TABLE "User" (
   "User_ID" INT GENERATED ALWAYS AS IDENTITY,
-  "User_Name",
-  "User_Email",
-  "User_Phone",
-  "Password_Hash",
-
--- Define Primary Key (PK) constraint
-
-
+  "User_Name" VARCHAR(100) NOT NULL,
+  "User_Email" VARCHAR(255) NOT NULL,
+  "User_Phone" VARCHAR(30),
+  "Password_Hash" VARCHAR(255) NOT NULL,
   
+  -- Data Integrity and Key Constraints
+  CONSTRAINT "PK_User" PRIMARY KEY ("User_ID"),
+  CONSTRAINT "UQ_User_Email" UNIQUE ("User_Email")
+);
 
 -- ============================================
 --           2. DEPENDENT ENTITIES
 -- ============================================
+
+CREATE TABLE "User_Profile" (
+  "Profile_ID" INT GENERATED ALWAYS AS IDENTITY,
+  "User_ID" INT NOT NULL,
+  "User_Bio" TEXT,
+  "Profile_Picture_URL" VARCHAR(255),
+
+  -- Primary Key (PK)
+  CONSTRAINT "PK_User_Profile" PRIMARY KEY ("Profile_ID"),
+
+  -- Foreign Key (FK) Child "User_Profile" Table referencing Parent "User" Table
+  CONSTRAINT "FK_User_Profile_User" FOREIGN KEY ("User_ID")
+    REFERENCES "User" ("User_ID")
+    ON DELETE CASCADE 
+
+
+
+
