2989aa6e57eef618a3060f8a3f8863c1c2d58f77
Ilkay Rachel Lee Sen <ilkaysen18@gmail.com>
2026-09-10 23:52:53 +0300

    Update 01_schema.sql

--- Development_P2/scripts/01_schema.sql
+++ Development_P2/scripts/01_schema.sql
@@ -1,8 +1,8 @@
 -- ============================================
---      1. BASE (INDEPENDENT) ENTITIES
+--       1. BASE (INDEPENDENT) ENTITY
 -- ============================================
 
--- Create main "User" Entity Table 
+-- Main "User" Entity Table 
 CREATE TABLE "User" (
   "User_ID" INT GENERATED ALWAYS AS IDENTITY,
   "User_Name" VARCHAR(100) NOT NULL,
@@ -16,7 +16,7 @@ CREATE TABLE "User" (
 );
 
 -- ============================================
---           2. DEPENDENT ENTITIES
+--           2. DEPENDENT ENTITITY
 -- ============================================
 
 CREATE TABLE "User_Profile" (
@@ -25,30 +25,48 @@ CREATE TABLE "User_Profile" (
   "User_Bio" TEXT,
   "Profile_Picture_URL" VARCHAR(255),
 
-  -- Primary Key (PK)
+  -- Define Primary Key (PK)
   CONSTRAINT "PK_User_Profile" PRIMARY KEY ("Profile_ID"),
 
-  -- Foreign Key (FK) Child "User_Profile" Table referencing Parent "User" Table
+  -- Foreign Key (FK) Child "User_Profile" Table references Parent "User" Table
   CONSTRAINT "FK_User_Profile_User" FOREIGN KEY ("User_ID")
     REFERENCES "User" ("User_ID")
     ON DELETE CASCADE 
 );
 
 -- ============================================
---             3. CORE ENTITIES
+--             3. CORE ENTITITY
 -- ============================================
 
 CREATE TABLE "Accommodation_Listing" (
   "Property_ID" INT GENERATED ALWAYS AS IDENTITY,
   "User_ID" INT NOT NULL,
-  "Accommodation_Address" VARCHAR(255) NOT NULL,
-  "Accommodation_Type" VARCHAR(50) NOT NULL, -- "Apartment" or "Bedroom"
-  "Accommodation_Description" TEXT,
-  "Max_Guests" INT NOT NULL DEFAULT 1,
+  "Accommodation_Address" VARCHAR(255) NOT NULL, -- new attribute added
+  "Accommodation_Type" VARCHAR(50) NOT NULL, -- "Apartment" or "Bedroom", newly added attribute
+  "Accommodation_Description" TEXT, -- new attribute added
+  "Max_Guests" INT NOT NULL DEFAULT 1, -- new attribute added
 
+  -- Define Primary Key (PK) and Constraints
+  CONSTRAINT "PK_Accommodation_Listing" PRIMARY KEY ("Property_ID"),
 
+  -- Foreign Key (FK) referencing Primary Key (PK)
+  CONSTRAINT "FK_Accommodation_Listing_User" FOREIGN KEY ("User_ID")
+    REFERENCES "User" ("User_ID")
+    ON DELETE CASCADE,
 
+  -- Domain rule constraint added to ensure Max_Guests attribute isn't recorded as below 0 or a negative valule
+  CONSTRAINT "CK_Max_Guests_Positive" CHECK ("Max_Guests" > 0)
+);
 
+-- ============================================
+--              4. CORE ENTITITY
+-- ============================================
+
+CREATE TABLE "Experience_Listing" (
+  "Experience_Listing_ID" INT GENERATED ALWAYS AS IDENTITY,
+  "User_ID" INT NOT NULL,
+  "Experience_Price" VALUE NOT NULL,
+  -- NEXT add some currency constraints and other constraints
 
 
 
