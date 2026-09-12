e4c765482379dd8191308733b21b63162c4eb50e
Ilkay Rachel Lee Sen <ilkaysen18@gmail.com>
2026-09-13 02:21:22 +0300

    Update 03_queries.sql

--- Development_P2/test_cases/03_queries.sql
+++ Development_P2/test_cases/03_queries.sql
@@ -1,1 +1,17 @@
+-- ===========================================================
+--        QUERY 1:    COMMISSION TRACKING AND REVENUE
+-- ===========================================================
+-- ALIASES: ft = Financial_Transaction, ab = Accommodation_Listing, eb = Experience_Booking, ap = Accommodation_Price, el = Experience_Listing
+
+
+-- ===========================================================
+--           QUERY 2:    ACCOMMODATION PERFORMANCE
+-- ===========================================================
+-- ALIASES: al = Accommodation_Listing, ar = Accommodation_Review
+
+
+-- ===========================================================
+--          QUERY 3:    SECURITY AND VERIFICATION
+-- ===========================================================
+-- ALIASES: 
 
