f379c3efeef6f5cfc7f65a571821335f48063506
Ilkay Rachel Lee Sen <ilkaysen18@gmail.com>
2026-09-12 23:33:43 +0300

    Update 02_dummy_data.sql

--- Development_P2/scripts/02_dummy_data.sql
+++ Development_P2/scripts/02_dummy_data.sql
@@ -1,31 +1,85 @@
--- ====================================================================          
+-- =============================================================================================          
 --       D U M M Y   D A T A :   2 0   U N I Q U E   R E C O R D S   -   U S I N G   D M L
--- ====================================================================          
+-- =============================================================================================          
 
 -- ============================
 --        BASE RECORDS
 -- ============================
 
+--  1  "User" Table
+
 -- ============================
 --  PROFILE & INVENTORY RECORDS
 -- ============================
+
+--  2  "User_Profile" Table
+
+--  3  "Verification" Table
+
+--  4  "Social_Media_Platform" Table
+
+--  5  "Payment_Method" Table
+
+--  6  "Accommodation_Listing" Table
+
+--  7  "Experience_Listing" Table
 
 -- ============================
 --  PRICING & CALENDAR RECORDS
 -- ============================
 
+--  8  "Accommodation_Price" Table
+
+--  9  "Amenity" Table
+
+--  10  "Property_Calendar" Table
+
+--  11  "Experience_Calendar" Table
+
 -- ============================
 -- SCHEDULING & BOOKING RECORDS
 -- ============================
+
+--  12  "Property_Block_Dates" Table
+
+--  13  "Experience_Block_Dates" Table
+
+--  14  "Accommodation_Booking" Table
+
+--  15  "Experience_Booking" Table
 
 -- ============================
 --    COMMUNICATIONS RECORDS
 -- ============================
 
+--  16  "Message_Thread" Table
+
+--  17  "Message_Log" Table
+
+--  18  "Notifications" Table
+
+--  19  "Financial_Transactions" Table
+
 -- ============================
 --   PAYOUTS & REVIEW RECORDS
 -- ============================
 
--- ====================================================================          
---    D M L   W I T H   D U M M Y   R E C O R D S   C O M P L E T E
--- ====================================================================          
+--  20  "Host_Payout" Table
+
+--  21  "Local_Payout" Table
+
+--  22  "Accommodation_Review" Table
+
+--  23  "Accommodation_Rating" Table
+
+--  24  "Experience_Review" Table
+
+--  25  "Experience_Rating" Table
+
+--  26  "User_Review" Table
+
+--  27  "User_Rating" Table
+
+-- =============================================================================================          
+--              D M L   W I T H   D U M M Y   R E C O R D S   C O M P L E T E
+-- =============================================================================================          
