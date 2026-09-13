a741c7da4bb3706e6cf46bf9333dec179647c8ef
Ilkay Rachel Lee Sen <ilkaysen18@gmail.com>
2026-09-13 18:57:26 +0300

    Update 01_1_schema.sql

--- Development_P2/scripts/01_1_schema.sql
+++ Development_P2/scripts/01_1_schema.sql
@@ -479,7 +479,7 @@ CREATE TABLE "Notifications" (
     REFERENCES "Experience_Booking" ("Experience_Booking_ID")
     ON DELETE CASCADE,
 
-  -- Domain Rule for (the above) OFK: Prevents booking from processing without at least 1 service, e.g. either Booking_ID or Experience_Booking_ID, depending on service booked by Guest
+  -- Domain Rule for (the above) OFK: Prevents notification from being sent out without at least 1 service, e.g. either Booking_ID or Experience_Booking_ID, depending on service booked by Guest
   CONSTRAINT "CK_Notifications_Source_Present" CHECK (
     ("Booking_ID" IS NOT NULL AND "Experience_Booking_ID" IS NULL) OR
     ("Booking_ID" IS NULL AND "Experience_Booking_ID" IS NOT NULL)
